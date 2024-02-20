91c00d2a1fa5c19c29f4cbe7ba43c5162bbdc5be
samfersosa <ssosam@unal.edu.co>
2024-02-19 20:33:35 -0500

    2

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ class Asiento:
     def __init__(self, color, precio, registro):
         self.color = color
         self.precio = precio
-        self.registro= registro
+        self.registro = registro
     
     def cambiarColor(self, newcolor):
-        if (newcolor == "blanco" or newcolor == "negro" or newcolor == "verde" or newcolor == "rojo" or newcolor == "amarillo"):
-           color == newcolor
+        if newcolor in ["blanco", "negro", "verde", "rojo", "amarillo"]:
+            self.color = newcolor
+
 class Auto:
     def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
         self.modelo = modelo
@@ -15,29 +16,30 @@ class Auto:
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        self.cantidadCreados =cantidadCreados
+        self.cantidadCreados = cantidadCreados
 
-        def cantidadAsientos(self):
-            return len(self.asientos)
+    def cantidadAsientos(self):
+        return len(self.asientos)
         
-        def verificarIntegridad(auto, motor):
-            registroAuto = auto.registro
-            registroMotor = motor.registro
-            asientosOriginales = all(asiento.registro == registroAuto for asiento in auto.asientos)
+    def verificarIntegridad(self, motor):
+        registroAuto = self.registro
+        registroMotor = motor.registro
+        asientosOriginales = all(asiento.registro == registroAuto for asiento in self.asientos)
 
-            if registroAuto == registroMotor and asientosOriginales:
-                return "Auto original"
-            else:
-                return "Las piezas no son originales"
+        if registroAuto == registroMotor and asientosOriginales:
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
+
 class Motor:
     def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
         self.tipo = tipo
         self.registro = registro
     
-        def cambiarRegistro(self, newregistro):
-            registro = newregistro
+    def cambiarRegistro(self, newregistro):
+        self.registro = newregistro
         
-        def asignarTipo(self, newtipo):
-            if (newtipo == "electrico" or newtipo == "gasolina"):
-                tipo = newtipo
+    def asignarTipo(self, newtipo):
+        if newtipo in ["electrico", "gasolina"]:
+            self.tipo = newtipo
